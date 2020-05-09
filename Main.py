2d7d00525647a380df7c0b49afe96cc189985816
psoczewka <piotrsoczewka92@gmail.com>
2020-05-09 18:53:53 +0200

    change2

--- Main.py
+++ Main.py
@@ -15,11 +15,30 @@ from sklearn.neighbors import KNeighborsClassifier
 
 class GroupProject:
     def dataPreprocessing(self):
-    self.train = pd.read_csv('Adult_train.tab', sep = '\t')
+        train = pd.read_csv('Adult_train.tab', sep = '\t', na_values = "?") #przy imporcie zamienia ? na NaN
+        pd.set_option("display.max_columns", None)
+        train = train.drop([0,1], axis=0) #usuwanie wierszy 0 i 1 tak jak ustaliliśmy, w jednej linijce
+        #print(train)
+        print(train.isnull().sum()) #w niektórych kolumnach jest prawie 2000 NaN
+        impFreq = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+        train_imp = impFreq.fit_transform(train)
+        train = DataFrame(train_imp, columns = list(train.columns))
+        print(train.isnull().sum()) #zamiana się udała, nie mamy teraz wartości NaN
+        print(train.dtypes) #wszystkie dane są typu obiektowego, których macierz korelacji nie widzi.
+        # Trzeba pozamieniać wartości nueryczne / logiczne.
+        # Najwięcej zabawy będzie z kolumną y - chcemy mieć bool, a mamy wszędzie wartości niezerowe,
+        # więc wszytko zostanie przekonwertowane na True.
+        # Używam więc SimpleImputer by zamienić '<=50K' na 0
+        impFreq = SimpleImputer(missing_values='<=50K', strategy='constant', fill_value = 0)
+        train_imp = impFreq.fit_transform(train)
+        train = DataFrame(train_imp, columns = list(train.columns))
+        train['y'] = train['y'].astype('bool') # zmiana typu danych na bool. 0 na False, dane niezmienione na True
+        train['age'] = train['age'].astype('float') #zmiana wieku na float. musi być tu, jak było przed SimpleImputer dla y to nie działało
+        print(train.dtypes) #widać, że age i y zostały zmienione
+        print(train.corr()) #macierz korelacji. Liczylem na większą korelację wieku z dochodem niż 23%. Trzeba zamienić więcej kolumn na typy numeryczne
 
 
-train = train.dropna()
-train = train.drop(0)
-impFreq = SimpleImputer(missing_values='?', strategy='most_frequent')
-train_imp = impFreq.fit_transform(train)
-train = DataFrame(train_imp, columns = list(train.columns))
+
+
+d = GroupProject()
+d.dataPreprocessing()
